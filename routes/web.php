030bee975dc73516aa920721eaf70951ec88406d
Radu Lepadatu <radulepa@gmail.com>
2022-04-05 21:02:04 +0300

    Update Web.php added 3 Routes
    /saveItemRoute
    /markCompleteRoute/{id}
    /markIncompleteRoute/{id}

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
+//*Import Controller:
+use App\Http\Controllers\TodoListController;
+
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +17,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//* // Default Route
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', [TodoListController::class, 'index']); //
+
+
+//* Add new item to DB route
+Route::post('/saveItemRoute', [TodoListController::class, 'saveItem'])->name('saveItem'); //name to point to the route
+
+//* Mark as Complete: Set is_complete = 1 to item ID
+Route::post('/markCompleteRoute/{id}', [TodoListController::class, 'markComplete'])->name('markComplete'); 
+
+//* Mark as In-Complete: Set is_complete = 0 to item ID
+Route::post('/markIncompleteRoute/{id}', [TodoListController::class, 'markIncomplete'])->name('markIncomplete'); 
